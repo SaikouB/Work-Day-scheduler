340e9fb6139818038b77a9d252b04e1f01ebb6ea
Smooth Operator <bsaikou1990@gmail.com>
2023-07-06 12:41:47 -0400

    Modified html/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,48 @@
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// $(document).ready(function () {
-
-// Sets current Date and Time at header of page down to the second and counting.
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-
-
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
 $(function () {
+  // Work day hour array to inlcude time, description, index hour and hour of day in 24hr format
   var workDayHour = [
-    { time: "9 AM", description: "", index: 'hour0' },
-    { time: "10 AM", description: "", index: 'hour1' },
-    { time: "11 AM", description: "", index: 'hour2' },
-    { time: "12 PM", description: "", index: 'hour3' },
-    { time: "1 PM", description: "", index: 'hour4' },
-    { time: "2 PM", description: "", index: 'hour5' },
-    { time: "3 PM", description: "", index: 'hour6' },
-    { time: "4 PM", description: "", index: 'hour7' },
-    { time: "5 PM", description: "", index: 'hour8' },
+    { time: "9 AM", description: "", index: 'hour0', hour: 9 },
+    { time: "10 AM", description: "", index: 'hour1', hour: 10 },
+    { time: "11 AM", description: "", index: 'hour2', hour: 11 },
+    { time: "12 PM", description: "", index: 'hour3', hour: 12 },
+    { time: "1 PM", description: "", index: 'hour4', hour: 13 },
+    { time: "2 PM", description: "", index: 'hour5', hour: 14 },
+    { time: "3 PM", description: "", index: 'hour6', hour: 15 },
+    { time: "4 PM", description: "", index: 'hour7', hour: 16 },
+    { time: "5 PM", description: "", index: 'hour8', hour: 17 }
   ];
 
-  var savedWorkDescription = JSON.parse(localStorage.getItem("workDayHour")) || workDayHour;
-
-  if (savedWorkDescription) {
-    workDayHour = savedWorkDescription;
-  }
-
-
+  // Work day hour loop for each time block row
   workDayHour.forEach(function (timeBlock, index) {
     var timeArea = timeBlock.time;
     var blockColor = colorRow(index);
-    var row =
-      `<div class="time-block${blockColor}" id="hour${index}" index="${index}">
-      <div class="row no-gutters">
-      <div class="col-2 col-md-1 hour text-center py-3">${timeArea}</div>
-      <textarea class="col-8 col-md-10 description" rows="3"></textarea>
-      <button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>
-      </div>
-      '</div>`;
+    var row = // Adds rows to html dynamically by template string
+      `<div class="time-block ${blockColor}" id="hour${index}" index="${index}">
+    <div class="row">
+    <div class="col-2 col-md-1 hour text-center py-3">${timeArea}</div>
+    <textarea class="col-8 col-md-10 description" rows="3"></textarea>
+    <button class="btn saveBtn col-2 col-md-1" aria-label="save"><i class="fas fa-save" aria-hidden="true"></i></button>
+    </div>
+    </div>`;
 
+    //Appends id of hour-container and adds rows for time-blocks under parent element "<div>"
     $("#hour-container").append(row);
-
   });
 
+  // Applys color to rows based on if past, present, or future, leveraged by CSS
   function colorRow(index) {
     var currentHour = dayjs().hour();
-    var hour = index + 9;
-    if (hour < currentHour) {
+    var blockHour = workDayHour[index].hour;
+    if (blockHour < currentHour) {
       return "past";
-    } else if (hour === currentHour) {
+    } else if (blockHour === currentHour) {
       return "present";
     } else {
-      return "future"
-    };
+      return "future";
+    }
   }
 
-  function updateTime() {
-    var now = dayjs();
-    var formattedDateTime = now.format('MMM DD, YYYY hh:mm:ss A');
-    $("#currentDayTime").text(formattedDateTime);
-  }
-  updateTime();
-  setInterval(updateTime, 1000);
-
+  // Adds click function to save button for each row using jquery and sets work-day hour to local storage
   $(document).on("click", ".saveBtn", function () {
     var timeBlock = $(this).closest(".time-block");
     var index = parseInt(timeBlock.attr("index"));
@@ -83,22 +51,25 @@ $(function () {
     localStorage.setItem("workDayHour", JSON.stringify(workDayHour));
   });
 
+  // Gets saved description from local storage
+  var savedWorkDescription = JSON.parse(localStorage.getItem("workDayHour")) || workDayHour.val();
 
-  for (let i = 0; i < savedWorkDescription.length; i++) {
+  // When page refreshes or reloads, adds saved description to time blocks from local storage
+  for (var i = 0; i < savedWorkDescription.length; i++) {
     var timeBlockId = savedWorkDescription[i].index;
     var timeBlock = $(`#${timeBlockId}`).closest(".time-block");
     var description = timeBlock.find(".description");
-    description.val(savedWorkDescription[i].description)
+    description.val(savedWorkDescription[i].description);
+    console.log()
   }
 
+  // Updates date and time live on webpage using 'MMM DD, YYYY hh:mm:ss A' dayjs format.
+  function updateTime() {
+    var now = dayjs();
+    var formattedDateTime = now.format('MMM DD, YYYY hh:mm:ss A');
+    $("#currentDayTime").text(formattedDateTime);
+  }
+  
+  updateTime(); // Calls function to update time
+  setInterval(updateTime, 1000); // 1 second time interval
 });
-
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-
-
-// save description data to local storage and have it persist when page is refreshed
-// set colors to past, present and future rows
